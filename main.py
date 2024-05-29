67db68a556ebcf1cfd50035ff3282599d358fb38
CRnk153 <arthurhard2009@gmail.com>
2024-05-29 21:07:21 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import nmap
+
+nmap_path = r""  # Write your path to Nmap folder
+nm = nmap.PortScanner(nmap_search_path=(nmap_path,))
+
+target_hosts = []  # Specify needed hosts to scan
+target_ports = ""  # Specify needed ports in the format "start_port-end_port" e.g. "22-443"
+
+def run():
+    nm.scan(target_hosts, arguments='-p 22-443')
+    for host in nm.all_hosts():
+        print('-----------------------------------------')
+        print(f'Хост: {host}')
+        print('Статус:', nm[host].state())
+
+        for proto in nm[host].all_protocols():
+            print('-----------------------------------------')
+            print(f'Протокол: {proto}')
+
+            ports = nm[host][proto].keys()
+            for port in ports:
+                state = nm[host][proto][port]['state']
+                service = nm[host][proto][port]['name']
+                print(f'Порт: {port}\tСтан: {state}\tСервіс: {service}')
+
+if __name__ == '__main__':
+    run()
---
+++ readME.md
@@ -0,0 +1,1 @@
+<h1><b>Extremely simple python-nmap script</b></h1>
